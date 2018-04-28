78f4f3582c00343415bbf21d53e68e40a15528b3
bi- <bi-@freemail.hu>
2018-04-28 08:44:15 +0200

    Initialize at once

--- vboxkc.sh
+++ vboxkc.sh
@@ -2,68 +2,14 @@
 # http://www.win.tue.nl/~aeb/linux/kbd/scancodes-1.html
 
 declare -A KEYMAP
-KEYMAP[-]=0C
-KEYMAP[ ]=39
-KEYMAP[a]=1E
-KEYMAP[b]=30
-KEYMAP[c]=2E
-KEYMAP[d]=20
-KEYMAP[e]=12
-KEYMAP[f]=21
-KEYMAP[g]=22
-KEYMAP[h]=23
-KEYMAP[i]=17
-KEYMAP[j]=24
-KEYMAP[k]=25
-KEYMAP[l]=26
-KEYMAP[m]=32
-KEYMAP[n]=31
-KEYMAP[o]=18
-KEYMAP[p]=19
-KEYMAP[q]=10
-KEYMAP[r]=13
-KEYMAP[s]=1F
-KEYMAP[t]=14
-KEYMAP[u]=16
-KEYMAP[x]=2D
-KEYMAP[y]=15
-KEYMAP[v]=2F
-KEYMAP[w]=11
-KEYMAP[z]=2C
-KEYMAP[enter]=1C
-KEYMAP[/]=35
+KEYMAP=( [-]=0C [ ]=39 [a]=1E [b]=30 [c]=2E [d]=20 [e]=12 [f]=21 [g]=22 [h]=23 [i]=17 [j]=24 \
+ [k]=25 [l]=26 [m]=32 [n]=31 [o]=18 [p]=19 [q]=10 [r]=13 [s]=1F [t]=14 [u]=16 [x]=2D [y]=15 \
+ [v]=2F [w]=11 [z]=2C [enter]=1C [/]=35 )
 
 declare -A RELEASE
-RELEASE[-]=8C
-RELEASE[ ]=B9
-RELEASE[a]=9E
-RELEASE[b]=B0
-RELEASE[c]=AE
-RELEASE[d]=A0
-RELEASE[e]=92
-RELEASE[f]=A1
-RELEASE[g]=A2
-RELEASE[h]=A3
-RELEASE[i]=97
-RELEASE[j]=A4
-RELEASE[k]=A5
-RELEASE[l]=A6
-RELEASE[m]=B2
-RELEASE[n]=B1
-RELEASE[o]=98
-RELEASE[p]=99
-RELEASE[q]=90
-RELEASE[r]=93
-RELEASE[s]=9F
-RELEASE[t]=94
-RELEASE[u]=96
-RELEASE[x]=AD
-RELEASE[y]=95
-RELEASE[v]=AF
-RELEASE[w]=91
-RELEASE[z]=AC
-RELEASE[enter]=9C
-RELEASE[/]=B5
+RELEASE=( [-]=8C [ ]=B9 [a]=9E [b]=B0 [c]=AE [d]=A0 [e]=92 [f]=A1 [g]=A2 [h]=A3 [i]=97 \
+[j]=A4 [k]=A5 [l]=A6 [m]=B2 [n]=B1 [o]=98 [p]=99 [q]=90 [r]=93 [s]=9F [t]=94 [u]=96 [x]=AD \
+[y]=95 [v]=AF [w]=91 [z]=AC [enter]=9C [/]=B5 )
 
 msg() {
   input=$1
